195c90f8252eff3e244514c429da7cc35df78920
Joshua Amaju <princejosh0007@gmail.com>
2024-01-24 23:07:02 +0100

    fix: html json parsing

--- packages/core/src/runtime/internal/html.ts
+++ packages/core/src/runtime/internal/html.ts
@@ -1,11 +1,12 @@
-import { uneval } from "devalue";
+import { parse, stringify } from "devalue";
 
 export function script_json(data: object, { id }: { id?: string }) {
-  return `<script id="${id}" type="application/json">${uneval(data)}</script>`;
+  const content = stringify(data);
+  return `<script id="${id}" type="application/json">${content}</script>`;
 }
 
 export function get_script_json(id: string): object | null {
   const element = document.getElementById(id);
   const content = element?.textContent;
-  return content ? JSON.parse(content) : null;
+  return content ? parse(content) : null;
 }
