4e9020d12b30073b5af097205331d504cb0691e9
Joshua Amaju <princejosh0007@gmail.com>
2024-01-24 20:22:54 +0100

    chore

--- packages/core/src/runtime/internal/html.ts
+++ packages/core/src/runtime/internal/html.ts
@@ -3,3 +3,9 @@ import { uneval } from "devalue";
 export function script_json(data: object, { id }: { id?: string }) {
   return `<script id="${id}" type="application/json">${uneval(data)}</script>`;
 }
+
+export function get_script_json(id: string): object | null {
+  const element = document.getElementById(id);
+  const content = element?.textContent;
+  return content ? JSON.parse(content) : null;
+}
