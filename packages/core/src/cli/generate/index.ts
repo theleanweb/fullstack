0e727410e9ca5b088c363083e1b57830ed603ddc
Joshua Amaju <princejosh0007@gmail.com>
2024-02-20 08:51:46 +0100

    chore

--- packages/core/src/cli/generate/index.ts
+++ packages/core/src/cli/generate/index.ts
@@ -1,10 +1,11 @@
 import { Command, Option } from "clipanion";
-import * as path from 'node:path'
+import * as path from "node:path";
+import * as fs from "node:fs";
 
-import * as tmpl from './templates'
+import * as tmpl from "./templates";
 
-const VIEWS_DIRECTORY = 'src/views/pages'
-const CONTROLLERS_DIRECTORY = 'src/controllers'
+const VIEWS_DIRECTORY = "src/views/pages";
+const CONTROLLERS_DIRECTORY = "src/controllers";
 
 export class GenerateCommand extends Command {
   static paths = [["g", "generate"]];
@@ -13,7 +14,25 @@ export class GenerateCommand extends Command {
 
   async execute() {
     const cwd = process.cwd();
-    const view = path.join(cwd, VIEWS_DIRECTORY, this.name)
-    const controller = path.join(cwd, CONTROLLERS_DIRECTORY, this.name)
+    const view = path.join(cwd, VIEWS_DIRECTORY, this.name);
+    const controller = path.join(cwd, CONTROLLERS_DIRECTORY, this.name);
+
+    const views = {
+      "index.svelte": tmpl.index(this.name),
+      "show.svelte": tmpl.show(this.name),
+      "new.svelte": tmpl.new_,
+    };
+
+    fs.writeFileSync(
+      path.join(controller, `${this.name}.controller.ts`),
+      tmpl.controller(this.name)
+    );
+
+    for (const name in views) {
+      const k = name as keyof typeof views;
+      fs.writeFileSync(path.join(view, name), views[k]);
+    }
+
+    fs.writeFileSync(path.join(controller, "utils.ts"), tmpl.utils(this.name));
   }
 }
--- packages/core/src/cli/generate/templates.ts
+++ packages/core/src/cli/generate/templates.ts
@@ -1,23 +1,100 @@
-export const controller = `
+export const controller = (name: string) => `
 import {Hono} from 'hono';
+
+import { renderSSR } from "@leanweb/fullstack/runtime/Render";
+
+import {edit, show} from './utils';
+
+import New from '@/views/pages/${name}/new.svelte?ssr';
+import Edit from '@/views/pages/${name}/edit.svelte?ssr';
+import Show from '@/views/pages/${name}/show.svelte?ssr';
+import Index from '@/views/pages/${name}/index.svelte?ssr';
 
 const router = new Hono();
 
-router.put('/', ctx => ctx.html(''));
-router.get('/', ctx => ctx.html(''));
-router.post('/', ctx => ctx.html(''));
-router.get('/:id', ctx => ctx.html(''));
-router.delete('/:id', ctx => ctx.html(''));
-`
+// Index
+router.get('/', ctx => ctx.html(renderSSR(Index)));
 
-export const list = `
-<script>
-export let list
-</script>
-`
+// New
+router.get('/new', ctx => ctx.html(renderSSR(New)));
 
-export const single = `
+// Create
+router.post('/', ctx => ctx.redirect('/${name}'));
+
+// Show
+router.get('/:id', ctx => ctx.html(renderSSR(Show)));
+
+// Edit
+router.get('/:id/edit', ctx => ctx.html(renderSSR(Edit)));
+
+// Update
+router.patch('/:id', ctx => ctx.redirect(show({id: ctx.req.param('id')})));
+
+// Delete
+router.delete('/:id', ctx => ctx.redirect('/${name}'));
+
+export const name = "${name}";
+
+export default router;
+`;
+
+export const index = (name: string) => /*html*/ `
 <script>
-export let item
+    import {show} from '@/controllers/${name}/utils'
+  export let ${name}_list;
 </script>
-`
+
+<ul>
+    {#each ${name}_list as ${name}}
+    <li>
+        <a href="{show({id: ${name}.id})}">{${name}.title}</a>
+    </li>
+    {/each}
+</ul>
+`;
+
+export const show = (name: string) => /*html*/ `
+<script>
+    import {edit, destroy} from '@/controllers/${name}/utils';
+    export let ${name};
+</script>
+
+<h1>{${name}.title}</h1>
+
+<p>{${name}.body}</p>
+
+<ul>
+  <li>
+    <a href="{edit({id: ${name}.id})}">Edit</a>
+  </li>
+
+  <li>
+    <a href="{destroy({id: ${name}.id})}">Destroy</a>
+  </li>
+</ul>
+`;
+
+export const new_ = /*html*/ `
+<form>
+    <div>
+        <label for="title">Title</label>
+        <input type="text" name="title" id="title">
+    </div>
+
+    <div>
+        <label for="body">Body</label>
+        <textarea name="body" id="body" cols="30" rows="10"></textarea>
+    </div>
+
+    <div>
+        <button type="submit">Submit</button>
+    </div>
+</form>
+`;
+
+export const utils = (name: string) => `
+import {compile} from "path-to-regexp";
+
+export const show = compile('${name}/:id');
+export const edit = compile('${name}/:id/edit');
+`;
