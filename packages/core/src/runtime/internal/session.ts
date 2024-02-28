d63d21ce12ba376ff1c6174357edd62f3e2a3792
Joshua Amaju <127889846+joshamaju@users.noreply.github.com>
2024-02-28 15:19:38 +0000

    feat: add session handling factory functions

---
+++ packages/core/src/runtime/internal/session.ts
@@ -0,0 +1,15 @@
+export type {
+  Session,
+  SessionData,
+  SessionIdStorageStrategy,
+  SessionStorage,
+} from "@remix-run/node";
+
+export {
+  createCookieSessionStorage,
+  createFileSessionStorage,
+  createMemorySessionStorage,
+  createSession,
+  createSessionStorage,
+  isSession,
+} from "@remix-run/node";
