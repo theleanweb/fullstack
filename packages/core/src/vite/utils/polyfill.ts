2d9366533b0707d95ef1e862287f13119996ecce
Joshua Amaju <princejosh0007@gmail.com>
2024-03-06 21:20:17 +0100

    refactor: use remove node polyfill

--- packages/core/src/vite/utils/polyfill.ts
+++ packages/core/src/vite/utils/polyfill.ts
@@ -1,18 +1,5 @@
-import {
-  File as NodeFile,
-  fetch as nodeFetch,
-  FormData as NodeFormData,
-  Headers as NodeHeaders,
-  Request as NodeRequest,
-  Response as NodeResponse,
-} from "@remix-run/web-fetch";
+import { installGlobals } from "@remix-run/node";
 
 export function installPolyfills() {
-  global.File = NodeFile;
-
-  global.Headers = NodeHeaders as unknown as typeof Headers;
-  global.Request = NodeRequest as typeof Request;
-  global.Response = NodeResponse as unknown as typeof Response;
-  global.fetch = nodeFetch as typeof fetch;
-  global.FormData = NodeFormData;
+  installGlobals();
 }
