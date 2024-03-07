5d7a291aaee6849f7da2b7e1fe7f75df934dd425
Joshua Amaju <127889846+joshamaju@users.noreply.github.com>
2024-03-07 11:53:00 +0000

    chore

--- playground/unocss/src/entry.ts
+++ playground/unocss/src/entry.ts
@@ -6,8 +6,8 @@ import { render } from "./setup/render.js";
 
 const app = new Hono();
 
-app.get("/", async (ctx) => ctx.html(await render("welcome", {})));
-app.get("/user", async (ctx) => ctx.html(await render("user/post", {})));
+app.get("/", (ctx) => ctx.html(render("welcome", {})));
+app.get("/user", (ctx) => ctx.html(render("user/post", {})));
 
 app.get('/user/:id', lazy(async () => (await import('./user.js')).show))
 
--- playground/unocss/src/setup/render.ts
+++ playground/unocss/src/setup/render.ts
@@ -4,7 +4,10 @@ import {
   type SSRComponentExport,
 } from "@leanweb/fullstack/runtime/Render";
 
-const components = import.meta.glob<SSRComponentExport>("../views/**/*.svelte");
+const components = import.meta.glob<SSRComponentExport>(
+  "../views/**/*.svelte",
+  { query: { ssr: true }, eager: true }
+);
 
 export const render = makeFactory((name) => {
   return resolveComponent(`../views/${name}.svelte`, components);
